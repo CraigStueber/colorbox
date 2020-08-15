37d7290261f87d3d81b0f7dcc12e0005b3854014
CraigStueber <craig@kurbmedia.com>
2020-08-15 17:41:37 -0400

    Working with more parent / childern and state

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import BoxContainer from './BoxContainer';
+
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <BoxContainer />
+    </div>
+  );
+}
+}
+
+export default App;
